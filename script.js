e35b286f209cc680c50d3aeb83f82f9a8b06c432
DjArtimus <pratik.s.pansare.in@gmail.com>
2024-08-05 05:10:09 +0530

    Updated the UI of the todo web app, added some more functionality and more features. Still some features not working.

--- script.js
+++ script.js
@@ -1,200 +1,389 @@
-// Variable to Set the serial count to 0
-let srNoCount = 1;
-const date = new Date();
+
+themeMode()
+
+const testDB = document.getElementsByClassName("testDB")[0];
+const signUpForm = document.getElementById("signUpForm");
+const loginForm = document.getElementById("loginForm");
+const toDoForm = document.getElementById("toDoForm");
+const toDoEditForm = document.getElementById("toDoEditForm");
+const closeForm = document.getElementById("closeForm");
+const closeLoginForm = document.getElementById("closeLoginForm");
+const toDoListDiv = document.getElementById("toDo_list");
+let toDoSrNoCounter = 1 ;
+
+createDataBase();
+
+const toDoDataBase = JSON.parse(localStorage.getItem("toDoDataBase"));
+const today = new Date().toLocaleString().split(",");
 
 
-function toDoList(priority=undefined){
-    let toDoList = [];
-    const toDoListDone = [];
+signUpForm.addEventListener("submit", (e) => {
+    const username = document.getElementById("username").value;
+    const users = Object.keys(toDoDataBase.users);
+    if(users.includes(username)){
+        document.getElementById("signUpAlert").innerHTML = `
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Username is already taken. <br> Please select different username
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        `
+        e.preventDefault();
+        
+    }
+    else{
+        createUser();
+        document.getElementById("signUpAlert").innerHTML = `
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Successfully Sign Up
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        `
 
-    for (let i = 0 ; i < localStorage.length ; i++ ){
-        let toDoId = localStorage.key(i);
-        if (toDoId.startsWith("d_")){
-            toDoListDone.push(toDoId);
+    }
+})
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const users = Object.keys(toDoDataBase.users);
+    if((toDoDataBase.currentUser[1]===username)&&(toDoDataBase.currentUser[2]===password)){
+        document.getElementById("loginAlert").innerHTML = `
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                You are already Loged In.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        `
+
+    }
+    else if(users.includes(username)){
+        e.preventDefault();
+        const userPassword = toDoDataBase.users[username][2];
+        if(password === userPassword){
+            toDoDataBase.currentUser = 
+            toDoDataBase.users[username];
+            document.getElementById("loginAlert").innerHTML = `
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Successfully Loged In
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                `
+            setTimeout(() => {
+                loginForm.reset();
+                closeLoginForm.click();
+            }, 1000);
+
         }
         else{
-            toDoList.push(toDoId);
+            document.getElementById("loginAlert").innerHTML = `
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                Wrong Password. Try Again
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        `
         }
     }
-
-    toDoList.sort();
-    toDoList.reverse();
-    document.getElementById("t").innerHTML= toDoList.length;
-
-    toDoList = priority==undefined? toDoList:priority;
-
-    for (let id of toDoList){
-        let toDoDetails = localStorage.getItem(id);
-            toDoDetails = toDoDetails.split(",");
-            let toDo = toDoDetails[0];
-            let desc = toDoDetails[1];
-            let type = toDoDetails[2];
-            createToDo(id,toDo,desc,type);
+    else{
+        document.getElementById("loginAlert").innerHTML = `
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Username not found ! Kindly Sign Up
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        `
+        
     }
-    for (let id of toDoListDone){
-        let check = localStorage.getItem(id);
-        createDone(id,check);
-    }
-}
+    // testDB.innerHTML += username + " " + password + " " + users.includes(username);
 
-let test = document.getElementsByClassName("test")[0];
-// function to add toDo
-function add(){
-    // Get the input values of to-dos from the user and stored in the variables.
-    const toDoId = String(date.getTime());
-    const toDo = document.getElementById("toDo").value;
+})
+
+
+toDoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
     const desc = document.getElementById("desc").value;
     const type = priority();
 
-    // test.innerHTML = toDoId + toDo + desc ;
-    // Save the inputs in local storage received from the user to access it even after refresh or end session or close tab.
-    // By using local storage we can access the data until we delete by manually.
-    // check for empty toDos and avoid them to save
-    if (toDo && (toDo || desc)){
+    addToDoToDataBase(title,desc,type);
 
-        localStorage.setItem(toDoId,[toDo,desc,type]);
-        
-        document.getElementById("toDo").value="";
-        document.getElementById("desc").value="";
-        document.getElementById("toDoWarn").innerHTML="";
-        location.reload(true);
-        // createToDo(toDoId,toDo,desc,type);
-    }
-    else{
-        document.getElementById("toDoWarn").innerHTML="Please fill out both fields Title and Description(optional)";
+    toDoForm.reset();
+    closeForm.click();
+})
+
+toDoEditForm.addEventListener("submit", () => {
+
+    const toDoId = document.getElementById("editTitle").getAttribute("data-toDoId");
+    const title = document.getElementById("editTitle").value;
+    const desc = document.getElementById("editDesc").value;
+    const type = editPriority();
+
+    editToDoToDataBase(toDoId,title,desc,type);
+    console.log(toDoId,title,desc,type)
+})
+
+
+function createDataBase(){
+    if(!(localStorage.getItem("toDoDataBase"))){
+        const toDoDB = {
+            currentUser : [],
+            users : {},
+            toDoListSortMethod : "Default" ,
+            usersToDoData : {}
+        }
+        localStorage.setItem("toDoDataBase",JSON.stringify(toDoDB))
     }
 }
+
+function createUser(){
+
+    const users = toDoDataBase.users;
+    const usersToDoData = toDoDataBase.usersToDoData;
+
+    const userDetails = {};
+    const uName = document.getElementById("name");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    userDetails.name = uName.value ;
+    userDetails.username = username.value ;
+    userDetails.password = password.value ;
+
+    users[username.value] = Object.values(userDetails);
+    
+    toDoDataBase.currentUser = Object.values(userDetails);
+
+    usersToDoData[username.value+"|s_toDoData"]={};
+
+    localStorage.setItem("toDoDataBase",JSON.stringify(toDoDataBase))
+}
+
+function createDatedToDoData(){
+    const user = toDoDataBase
+    .currentUser[1] + "|s_toDoData";
+
+    const datedToDoData = Object.keys(toDoDataBase.usersToDoData[user]).includes(today[0]);
+    if(!datedToDoData){
+        toDoDataBase.usersToDoData[user][today[0]] = {};
+        localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase))
+    }
+}
+
+function addToDoToDataBase(title,desc,type,isDone = false){
+    try {
+        
+        const usersToDoData = toDoDataBase.usersToDoData;
+        const user = toDoDataBase.currentUser[1];
+        
+        const userData = usersToDoData[user+"|s_toDoData"];
+        createDatedToDoData();
+        const toDoId = new Date().getTime();
+        userData[today[0]][toDoId] = [toDoId,title,desc,type,isDone,today[1]];
+        localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase));
+        location.reload(true);
+
+    } catch (error) {
+        testDB.innerHTML = "You are not loged in. <br> Please Login or Sign-Up to create your To dos" ;
+    }
+    }
 
 function priority(){
-    if (document.getElementById("imp").checked){
-        return "imp";
+    if (document.getElementById("prime").checked){
+        return "prime";
     }
-    else if (document.getElementById("gen").checked){
-        return "gen";
+    else if (document.getElementById("power").checked){
+        return "power";
     }
-    else { return "norm"; }
+    else { return "lite"; }
+}
+function editPriority(){
+    if (document.getElementById("edit_prime").checked){
+        return "prime";
+    }
+    else if (document.getElementById("edit_power").checked){
+        return "power";
+    }
+    else { return "lite"; }
 }
 
-// function to create toDo
-function createToDo(toDoId,toDo,desc,type){
 
-    const toDoList = document.getElementById("toDo_list");
-    const divId = "srNo_" + toDoId;
-    const doneID = "d_" + toDoId;
+function createToDo(id,title,desc,type,isDone,time){
 
     let typeOfTodo;
-    if(type=="imp"){
+    if(type=="prime"){
         typeOfTodo = "alert-danger";
     }
-    else if(type=="gen"){
-        typeOfTodo = "alert-warning text-warning";
+    else if(type=="power"){
+        typeOfTodo = "alert-primary";
     }
     else{
-        typeOfTodo = "alert-primary";
+        typeOfTodo = "alert-warning";
+    }
+
+    let check = undefined;
+    if (isDone){
+        check = "checked";
     }
 
     let toDoHTML = `
-                <div id="${divId}" class="alert ${typeOfTodo} ${type} toDoElement card" role="alert">
-                  <div class="srNos card-header">${srNoCount}</div>
-                  <div class="card-body">
-                    <h3 class="title card-title">${toDo}</h3>
-                    <p class="description card-text">${desc}</p>
-                    <div class="form-check form-switch d-inline-block">
-                    <input type="checkbox" id="${doneID}" class="done form-check-input"
-                      onclick="done(this.id)">
-                    <label for="${doneID}" class="doneLabel form-check-label">Mark as DONE </label>
+                <div id="${id}" class=" rounded-3 p-2  alert ${typeOfTodo} ${type} toDoElement card" role="alert">
+    
+                  <div class="srNos p-1 px-2 card-header d-flex justify-content-between bg-transpa rent">
+                
+                  <div class="d-inline-block pt-1 px-1">
+                    ${toDoSrNoCounter} 
+                    <span class=" opacity-25" >↓</span> ${time} 
                     </div>
-                    <button id="${toDoId}" class="del btn btn-danger" onclick="del(this.id)">Delete</button>
+                    <div class="d-inline-block">
+                    
+                  <i id="${"editId_" + id}" onclick="edit(this.id)" class=" p-0 px-1 bi bi-pencil-square btn" data-bs-toggle="modal" data-bs-target="#editToDoModal" data-bs-whatever="@mdo"></i><i id="${"delId_" + id}" onclick="del(this.id)" class="p-1 bi bi-trash3-fill btn"></i>
                   </div>
+                  </div>
+                  <div class="card-body ">
+                    <h3 class="title card-title">${title}</h3>
+                    <p class="description card-text">${desc}<input type="checkbox" id="${"markId_" + id}" class=" float-end done form-check-input border-info border-opacity-50"
+                    onclick="done(this.id)" style="font-size:14px; transform:translateX(6px) translateY(12px); cursor:pointer;" ${check}></p>
+                    
+                  </div>
+                  
                 </div>
     `
 
-    toDoList.innerHTML += toDoHTML;
- 
-    srNoCount++;//increase SrNo counter
+    toDoListDiv.innerHTML += toDoHTML;
+    createDone(id,isDone);
+    toDoSrNoCounter++;//increase SrNo counter
 }
 
-// function to del to do
-function del(toDo){
-    document.getElementById(("srNo_"+toDo)).style.display = "none";
-    localStorage.removeItem(toDo);
-    localStorage.removeItem(("d_"+toDo));
-    location.reload(true);
+function done(id){
+    let check = document.getElementById(id).checked;
+    id=id.split("_")[1];
+    const currentUserData = 
+    toDoDataBase.currentUser[1] + "|s_toDoData";
+
+    if(check){
+        toDoDataBase
+        .usersToDoData[currentUserData]
+        [today[0]][id][4] = check;
+        localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase));
+    }
+    else{
+        toDoDataBase
+        .usersToDoData[currentUserData]
+        [today[0]][id][4] = check;
+        localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase));
+    }
+
+    createDone(id,check);       
 }
 
-// funtion to del all to dos
-function delAll (){
-    localStorage.clear();
-    location.reload(true);
-}
-// funtion to create mark down done or undone and change the appearance of to do element
 function createDone(id,check){
-    let checkId = document.getElementById(id);
+    let markId= "markId_" + id;
+    let checkId = document.getElementById(markId);
     checkId.checked = check;
-    id=id.split("_");
-    let markId="srNo_"+id[1];
-    let mark = document.getElementById(markId);
-    let markLable = mark.childNodes;
+    let mark = document.getElementById(id);
+    // let markLable = mark.childNodes;
     
     if (check){
-        mark.style.background = "black";
-        mark.style.opacity = "0.7";
-        markLable[3].childNodes[5].childNodes[3].innerHTML = "Marked as DONE";
+        if(window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches){
+        mark.style.background = "black";}
+        else{
+        mark.style.background = "whitesmoke";}
+        mark.style.opacity = "0.8";
+        // markLable[3].childNodes[5].childNodes[3].innerHTML = "Marked as DONE";
     }
     else{
         mark.style.background = "";
         mark.style.opacity = "1";
-        markLable[3].childNodes[5].childNodes[3].innerHTML = "Mark as DONE";
+        // markLable[3].childNodes[5].childNodes[3].innerHTML = "Mark as DONE";
     }
 }
 
-var t2 = document.getElementById("t2");
-// function to trigger mark done or undone and add to local storage
-function done(id){
-    let check = document.getElementById(id).checked;
-    if(check){
-        localStorage.setItem(id,check);
+function del(id){
+    const currentUserData = 
+    toDoDataBase.currentUser[1] + "|s_toDoData";
+    id = id.split("_")[1];
+
+    toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][id] = undefined ;
+    localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase));
+    location.reload(true);
+}
+
+function edit(id){
+    const currentUserData = 
+    toDoDataBase.currentUser[1] + "|s_toDoData";
+    id = id.split("_")[1];
+    const type = "edit_" + toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][id][3];
+
+    let editTitle = document.getElementById("editTitle");
+    let editDesc = document.getElementById("editDesc");
+    const editType = document.getElementById(type);
+
+    editTitle.value = toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][id][1];
+    editDesc.value = toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][id][2];
+    editType.setAttribute("checked",true);
+
+    editTitle.setAttribute("data-toDoId",id);
+
+}
+
+function editToDoToDataBase(toDoId,title,desc,type){
+    const currentUserData = 
+    toDoDataBase.currentUser[1] + "|s_toDoData";
+
+    toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][toDoId][1] = title ;
+    toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][toDoId][2] = desc ;
+    toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]][toDoId][3] = type ;
+    localStorage.setItem("toDoDataBase", JSON.stringify(toDoDataBase));
+    location.reload(true);
+
+}
+
+try {
+    const currentUserData = 
+    toDoDataBase.currentUser[1] + "|s_toDoData";
+    const todaysToDoData = toDoDataBase
+    .usersToDoData[currentUserData]
+    [today[0]];
+            
+    for (let todoId in todaysToDoData ){
+        const todo = todaysToDoData[todoId];
+        let id = todo[0];
+        let title = todo[1];
+        let desc = todo[2];
+        let type = todo[3];
+        let isDone = todo[4];
+        let time = todo[5];
+        
+        createToDo(id,title,desc,type,isDone,time);
+
+
+    }
+} catch (error) {
+    
+}
+
+function themeMode(){
+    if(window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches){
+        document.body.setAttribute("data-bs-theme","dark");
     }
     else{
-        localStorage.removeItem(id);
-    }
-    createDone(id,check);    
-}
-
-
-function prioritySort(evt){
-    if(evt.target.value==="Priorities"){
-
-        // let Priority = document.getElementById("prioritySort").children;
-        // let setPriority = Priority[1];
-        // setPriority.setAttribute(selected);
-
-        const priorityList = [];
-        let toDos = document.getElementById("toDo_list").children;
-        
-        for (let toDo of toDos){
-            let type = toDo.classList;
-            let id = toDo.id;
-            if (type.contains("imp")){
-                priorityList.push("c"+id);
-            }
-            else if (type.contains("gen")){
-                priorityList.push("b"+id);
-            }
-            else if (type.contains("norm")){
-                priorityList.push("a"+id);
-            }
-        }
-
-        priorityList.sort();
-        priorityList.reverse();
-
-        const priorityIds = [];
-        for (let pLId of priorityList){
-            let id = pLId.split("_");
-            priorityIds.push(id[1]);
-        }
-        // document.getElementById("temp").innerHTML= priorityList.length + " " + priorityList + "<br>" + priorityIds ;
-        toDoList(priorityIds);
+        document.body.removeAttribute("data-bs-theme");
     }
 }
+
+
+console.log("all good");
+
+testDB.innerHTML= `<div style="width:80px; text-align:center; line-height:17px; text-transform:uppercase;">${new Date().toDateString()}</div>`
